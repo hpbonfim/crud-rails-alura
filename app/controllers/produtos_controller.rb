e900bb9fb44fbe5c345381f6f5084d8a2454e4ac
Henrique Bonfim <74271969+henriquebonfim@users.noreply.github.com>
2021-05-16 18:28:42 -0300

    create

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -4,4 +4,10 @@ class ProdutosController < ApplicationController
         @produtos = Produto.order(nome: :desc).limit 2
         @produto_com_desconto = Produto.order(:preco).limit 1
     end
+   
+    def create
+      produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+      Produto.create produto
+      redirect_to root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "produtos#index"
-  get "produtos", to: "produtos#index"
+  get "produtos/create", to: "produtos#new"
+  post "produtos", to: "produtos#create"
 end
