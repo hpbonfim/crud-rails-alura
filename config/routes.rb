ef573151f4bd1730ebe1527e6f7bba74a5dad407
Henrique Bonfim <74271969+henriquebonfim@users.noreply.github.com>
2021-05-16 19:14:23 -0300

    search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "produtos#index"
-  get "produtos/create", to: "produtos#new"
-  post "produtos", to: "produtos#create"
-  delete "produtos/:id/remover", to: "produtos#destroy", as: :produto
+  resources :produtos, only: [:new, :create, :destroy]
+  get "produtos/busca", to: "produtos#busca", as: :busca_produto
+
 end
